9e3f3dffb3b1c82d2db192665a6dc13fd6156967
Zac <13089@holbertonstudents.com>
2026-02-10 05:01:07 +0000

    random

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+#include <stdio.h>
 
-/* betty style doc for function main goes there */
+/*
+ * main - random number generator
+ *
+ * return - 0 is true
+ */
 int main(void)
 {
 	int n;
