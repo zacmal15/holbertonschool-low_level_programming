0117bc687b1cd74ca6f0bfbc046f1e08bdb85f67
Zac <13089@holbertonstudents.com>
2026-02-11 00:40:26 +0000

    backwards

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print aphabet backwards
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
