9b1b7c5d75144822dda1da797a7a9b81d5c8225c
Zac <13089@holbertonstudents.com>
2026-02-12 10:24:18 +0000

    adding-absolute-file

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - prints absolute integer
+ * @n: number to compute
+ *
+ * Return: absolute value of n
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+
+	return (n);
+}
