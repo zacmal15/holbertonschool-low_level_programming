70f6f8093185bd95076d4cd91a421df5e02bd86d
Zac <13089@holbertonstudents.com>
2026-02-16 07:13:00 +0000

    printline

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in terminal
+ * @n: number of times the character '_' should be printed
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+			_putchar('_');
+	}
+
+	_putchar('\n');
+}
