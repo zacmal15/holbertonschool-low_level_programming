bc823ec51c36cf5d7ba568fb7177983bdcff0125
Zac <13089@holbertonstudents.com>
2026-02-17 01:48:37 +0000

    string

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - returns length of a string
+ * @s: pointer to the length of string
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int length;
+
+	length = 0;
+
+	while (s[length] != '\0')
+	{
+		length++
+	}
+
+	return (length);
+}
