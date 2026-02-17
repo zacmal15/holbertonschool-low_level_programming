7dc702ae5a065b2d7021d1241920f4a1f0320df2
Zac <13089@holbertonstudents.com>
2026-02-17 01:23:18 +0000

    reset98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates value of int
+ * stored at the address of the value of n to 98
+ * @n: pointer to the int to update
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
