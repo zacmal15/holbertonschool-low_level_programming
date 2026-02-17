40cf86988a4e9e6da5c067e67eb1fb25fc072984
Zac <13089@holbertonstudents.com>
2026-02-17 02:38:40 +0000

    reversedstring

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses string
+ * @s: points to string to reverse
+ */
+void rev_string(char *s)
+{
+	int length;
+	int i;
+	char temp;
+
+	length = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	for (i = 0; i < length / d; i++)
+	{
+		temp = s[i];
+		s[i] = s[length - 1 - i];
+		s[length - 1 - i] = temp;
+	}
+}
