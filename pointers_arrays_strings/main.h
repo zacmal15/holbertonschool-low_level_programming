56f2f540259f9adef9957134a33099d045f9576d
Zac <13089@holbertonstudents.com>
2026-02-17 03:56:54 +0000

    fix

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -13,6 +13,6 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-char *_strcat(char *dest. char *src);
+char *_strcat(char *dest, char *src);
 
 #endif
