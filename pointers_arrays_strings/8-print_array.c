d40de1d137a6fae9686f49b0af2c21459493593a
Zac <13089@holbertonstudents.com>
2026-02-17 03:12:06 +0000

    printarray

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_array - print array of integers
+ * @a: pointer to first element of array
+ * @n: number of elements to print
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+
+		if (i < n - 1)
+		{
+			printf(", ");
+		}
+	}
+
+	printf("\n");
+}
