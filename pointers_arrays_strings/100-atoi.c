ea5c61801c1635e80653c7fbca92fb8b049fcd91
Zac <13089@holbertonstudents.com>
2026-02-18 07:57:45 +0000

    atoi

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,1 +1,39 @@
+#include "main.h"
 
+/**
+ * _atoi - converts string to int
+ * @s: string to convert
+ *
+ * Return: int value
+ */
+int _atoi(char *s)
+{
+	int sign = 1;
+	int result = 0;
+	int started = 0;
+
+	while (*s != '\0')
+	{
+		if (!started && (*s == '-' || *s == '+'))
+		{
+			if (*s == '-')
+				sign = -sign;
+		}
+		else if (*s > '0' && *s <= '9')
+		{
+			started = 1;
+			result = result * 10 + (*s - '0');
+		}
+		else if (started)
+		{
+			break;
+		}
+
+		s++;
+	}
+
+	if (!started)
+		return (0);
+
+	return (result * sign);
+}
