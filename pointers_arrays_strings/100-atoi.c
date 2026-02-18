18aaad6b37b3484d5e12765c201da4370ef9a91c
Zac <13089@holbertonstudents.com>
2026-02-18 22:05:13 +0000

    atoi

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,70 +1,43 @@
 #include "main.h"
 
 /**
- * parse_number - extracts digits and determines sign
- * @s: input tring
- * @sign: pointer to sign multiplier
- * @started: pointer to digit start flag
- *
- * Return: parsed unsigned int value
- */
-static unsigned int parse_number(char *s, int *sign, int *started)
-{
-	unsigned int result = 0;
-
-	while (*s != '\0')
-	{
-		if (!(*started) && (*s == '-' || *s == '+'))
-		{
-			if (*s == '-')
-				*sign = -(*sign);
-		}
-		else if (*s >= '0' && *s <= '9')
-		{
-			*started = 1;
-			if (result > 214748364U)
-				result = 2147483647U;
-			else
-			{
-				result = result * 10 + (unsigned int)(*s - '0');
-				if (result > 2147483647U)
-					result = 2147483647U;
-			}
-		}
-		else if (*started)
-		{
-			break;
-		}
-		s++;
-	}
-
-	return (result);
-}
-
-/**
  * _atoi - converts string to int
  * @s: string to convert
  *
- * Return: converted int value
+ * Return: int value found in string, or 0 if none
  */
-
 int _atoi(char *s)
 {
-	int sign = 1;
-	int started = 0;
-	unsigned int result;
+	int i;
+	int sign;
+	unsigned int num;
+	int started;
 
-	result = parse_number(s, &sign, &started);
+	i = 0;
+	sign = 1;
+	num = 0;
+	started = 0;
 
-	if (!started)
-		return (0);
-
-	if (sign == -1)
+	while (s[i] != '\0')
 	{
-		if (result >= 2147483648U)
-			return (-2147483648);
-		return (-(int)result);
+		if (s[i] == '-')
+			sign *= -1;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			started = 1;
+			num = (num * 10) + (s[i] - '0');
+		}
+		else if (started)
+		{
+			break;
+		}
+
+		i++;
 	}
 
-	return ((int)result);
+	if (sign < 0)
+		return (-num);
+
+	return (num);
 }
