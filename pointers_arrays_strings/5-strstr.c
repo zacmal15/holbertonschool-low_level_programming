7eff1e2d0ff1e74ea459059b38c340e35c2566f0
Zac <13089@holbertonstudents.com>
2026-02-23 01:26:45 +0000

    strstr

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strstr - locates substring
+ * @haystack: string to search in
+ * @needle: substring to find
+ *
+ * Return: pointer to beginning of located substring
+ * or NULL if not found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	if (needle[0] == '\0')
+		return (haystack);
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		j = 0;
+
+		while (needle[j] != '\0' && haystack[i + j] == needle[j])
+		{
+			j++
+		}
+
+		if (needle[j] == '\0')
+			return (haystack + i);
+	}
+
+	return (NULL);
+}
