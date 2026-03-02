395908d56b659eb7bd8cd1513a562f65dcf3cf0c
Zac <13089@holbertonstudents.com>
2026-03-02 04:37:24 +0000

    fixed

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -32,7 +32,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n >= len2)
 		n = len2;
 
-	new str = malloc(sizeof(char) * (len1 + n + 1));
+	new_str = malloc(sizeof(char) * (len1 + n + 1));
 
 	if (new_str == NULL)
 		return (NULL);
