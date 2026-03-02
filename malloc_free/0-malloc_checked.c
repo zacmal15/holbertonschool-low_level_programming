727bcff69c335bbcb50a36333450b844caf4aaf1
Zac <13089@holbertonstudents.com>
2026-03-02 04:12:21 +0000

    checkmalloc

--- malloc_free/0-malloc_checked.c
+++ malloc_free/0-malloc_checked.c
@@ -12,10 +12,10 @@ void *malloc_checked(unsigned int b)
 {
 	void *ptr;
 
-	ptr = malloc(b)
+	ptr = malloc(b);
 
-		if (ptr == NULL)
-			exit(98);
+	if (ptr == NULL)
+		exit(98);
 
 	return (ptr);
 }
