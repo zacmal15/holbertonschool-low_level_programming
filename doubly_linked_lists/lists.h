80d8b5a181e7aa57a19fc6c1e355dd88c648d8be
Zac <13089@holbertonstudents.com>
2026-03-24 00:24:12 +0000

    printdlist

---
+++ doubly_linked_lists/lists.h
@@ -0,0 +1,24 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struct dlistin_s - doubly linked list node
+ * @n: int stored in node
+ * @prev: points to previous node
+ * @next: points to next node
+ *
+ * Description: doubly linked list node structure
+ */
+typedef struct dlistin_s
+{
+	int n;
+	struct dlistin_s *prev;
+	struct dlistint_s *next;
+} dlistint_t;
+
+size_t print_dlistin(const dlistin_t *h);
+
+#endif
