b1b2ff52340998c3615fde2343040a5489cd28e7
Zac <13089@holbertonstudents.com>
2026-03-24 01:14:01 +0000

    freelist

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees dlistint_list
+ * @head: pointer to head of list
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
