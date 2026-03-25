23f6cd4df944e5d6b748610ac21d98f6e8743f4f
Zac <13089@holbertonstudents.com>
2026-03-25 01:16:03 +0000

    fix

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -38,6 +38,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	if (bytes_written != bytes_read)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
 	free(buffer);
 	close(fd);
 
