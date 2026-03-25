e146560f6b3e1747493e3dc028981dea2006128f
Zac <13089@holbertonstudents.com>
2026-03-25 01:09:37 +0000

    readtextfile

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -13,7 +13,7 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t bytes_read, bytes_written;
+	ssize_t bytes_read, bytes_written = 0;
 	char *buffer;
 
 	if (filename == NULL)
